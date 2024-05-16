afabc61ac4c9a72bc4dde98b10c716bb1ad1bbab
Herik Santos <heriksantos04@gmail.com>
2024-05-16 19:23:12 -0300

    Mudanças de ambiente

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,18 @@
+import dotenv from "dotenv";
+import path from "path";
 import { DataSource } from "typeorm";
+
+dotenv.config({
+    path: path.resolve(__dirname, "..", "..", ".env.dev"),
+});
 
 const appDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: process.env.DATABASE_HOST,
     port: 3306,
-    database: "barbearia",
-    username: "root",
-    password: "123456",
+    database: process.env.DATABASE,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
     synchronize: true,
     logging: true,
     entities: ["./src/database/entities/*.ts"],
