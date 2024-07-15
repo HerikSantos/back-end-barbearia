a8681b550d118172bd17f469d7568b0217fd5719
Herik Santos <heriksantos04@gmail.com>
2024-07-15 14:17:16 -0300

    Revert "refactor: Changes test create and delete"
    This reverts commit 4aa8c39af4f203b8d5fac1d47808b121cfc90ee7.

--- src/useCases/Admins/CreateAdminUseCase/CreateAdminUseCase.spec.ts
+++ src/useCases/Admins/CreateAdminUseCase/CreateAdminUseCase.spec.ts
@@ -1,109 +1,52 @@
+import { AppError } from "../../../errors/AppErros";
 import { AdminsRepositoryMemory } from "../../../repositories/AdminsRepositoryMemory";
-import { type IAdminsRepository } from "../../../repositories/IAdminsRepository";
-import { cryptPassword } from "../../../utils/cryptPassword";
 import { CreateAdminUseCase } from "./CreateAdminUseCase";
 
-interface ISutTypes {
-    adminsRepository: IAdminsRepository;
-    sut: CreateAdminUseCase;
-}
-function makeSut(): ISutTypes {
-    const adminsRepository = new AdminsRepositoryMemory();
-    const sut = new CreateAdminUseCase(adminsRepository);
-
-    return {
-        adminsRepository,
-        sut,
-    };
-}
-
-jest.mock("../../../utils/cryptPassword", () => ({
-    cryptPassword: jest.fn(),
-}));
+let createAdminUseCase: CreateAdminUseCase;
+let adminsRepository: AdminsRepositoryMemory;
 
 describe("Create a new admin", () => {
-    it("Should be alble create a admin", async () => {
-        const { sut, adminsRepository } = makeSut();
+    beforeEach(() => {
+        adminsRepository = new AdminsRepositoryMemory();
+        createAdminUseCase = new CreateAdminUseCase(adminsRepository);
+    });
 
+    it("Should be alble create a admin", async () => {
         const admin = {
             name: "teste da silva",
             email: "testando@gmail.com",
             password: "teste",
         };
-        await sut.execute(admin);
+        await createAdminUseCase.execute(admin);
 
         const createdAdmin = await adminsRepository.findOne(admin);
 
         expect(createdAdmin).toHaveProperty("id");
     });
 
-    it("Should be call create with correct params", async () => {
-        const { sut, adminsRepository } = makeSut();
-
-        const admin = {
-            name: "teste da silva",
-            email: "testando@gmail.com",
-            password: "teste",
-        };
-
-        (cryptPassword as jest.Mock).mockResolvedValue("hashed_password");
-
-        const creteAdminUseCaseSpy = jest.spyOn(adminsRepository, "create");
-
-        await sut.execute(admin);
-
-        expect(creteAdminUseCaseSpy).toHaveBeenCalledWith({
-            name: "teste da silva",
-            email: "testando@gmail.com",
-            password: "hashed_password",
-        });
-    });
-
-    it("Should be call cryptPassword with correct params", async () => {
-        const { sut } = makeSut();
-
-        const admin = {
-            name: "teste da silva",
-            email: "testando@gmail.com",
-            password: "teste",
-        };
-
-        await sut.execute(admin);
-
-        expect(cryptPassword).toHaveBeenCalledWith(admin.password);
-    });
-
     it("Should be impossible create a admin with email invalid", async () => {
-        const { sut } = makeSut();
-        const admin = {
-            name: "teste da silva",
-            email: "testando.com",
-            password: "teste",
-        };
         await expect(async () => {
-            await sut.execute(admin);
-        }).rejects.toMatchObject({
-            message: "Email is not valid",
-            statusCode: 400,
-        });
+            const admin = {
+                name: "teste da silva",
+                email: "testando.com",
+                password: "teste",
+            };
+
+            await createAdminUseCase.execute(admin);
+        }).rejects.toBeInstanceOf(AppError);
     });
 
     it("Should be impossible create a admin with email already exists", async () => {
-        const { sut } = makeSut();
-
-        const admin = {
-            name: "teste da silva",
-            email: "teste@gmail.com",
-            password: "teste",
-        };
-
-        await sut.execute(admin);
-
         await expect(async () => {
-            await sut.execute(admin);
-        }).rejects.toMatchObject({
-            message: "Email is not valid",
-            statusCode: 400,
-        });
+            const admin = {
+                name: "teste da silva",
+                email: "teste@gmail.com",
+                password: "teste",
+            };
+
+            await createAdminUseCase.execute(admin);
+
+            await createAdminUseCase.execute(admin);
+        }).rejects.toBeInstanceOf(AppError);
     });
 });
--- src/useCases/Admins/DeleteAdminUseCase/DeleteAdminUseCase.spec.ts
+++ src/useCases/Admins/DeleteAdminUseCase/DeleteAdminUseCase.spec.ts
@@ -2,26 +2,12 @@ import { v4 as uuidv4 } from "uuid";
 
 import { AppError } from "../../../errors/AppErros";
 import { AdminsRepositoryMemory } from "../../../repositories/AdminsRepositoryMemory";
-import { type IAdminsRepository } from "../../../repositories/IAdminsRepository";
 import { DeleteAdminUseCase } from "./DeleteAdminUseCase";
-
-interface ISutTypes {
-    adminsRepository: IAdminsRepository;
-    sut: DeleteAdminUseCase;
-}
-function makeSut(): ISutTypes {
-    const adminsRepository = new AdminsRepositoryMemory();
-    const sut = new DeleteAdminUseCase(adminsRepository);
-
-    return {
-        adminsRepository,
-        sut,
-    };
-}
 
 describe("Delete admin", () => {
     it("Should be able delete a admin", async () => {
-        const { sut, adminsRepository } = makeSut();
+        const adminsRepository = new AdminsRepositoryMemory();
+        const deleteAdminUseCase = new DeleteAdminUseCase(adminsRepository);
 
         const admin = {
             name: "teste",
@@ -35,16 +21,17 @@ describe("Delete admin", () => {
 
         if (!result) throw new AppError("Error Test delete");
 
-        await sut.execute({ id: result.id });
+        await deleteAdminUseCase.execute({ id: result.id });
 
         const existedAdmin = await adminsRepository.findByID(result.id);
 
         expect(existedAdmin).toBeNull();
     });
 
-    it("Should return a throw if admin do not exists", async () => {
+    it("Should be impossible delete a inexistent admin", async () => {
         await expect(async () => {
-            const { sut } = makeSut();
+            const adminsRepository = new AdminsRepositoryMemory();
+            const deleteAdminUseCase = new DeleteAdminUseCase(adminsRepository);
 
             const admin = {
                 id: uuidv4(),
@@ -53,42 +40,7 @@ describe("Delete admin", () => {
                 password: "123teste",
             };
 
-            await sut.execute({ id: admin.id });
-        }).rejects.toMatchObject({
-            message: "Admin not found",
-            statusCode: 400,
-        });
-    });
-
-    it("Ensure call findByID with correct param", async () => {
-        const { sut, adminsRepository } = makeSut();
-        // eslint-disable-next-line
-
-        const admin = {
-            id: "valid_id",
-            name: "valid_name",
-            email: "valid_email",
-            password: "valid_password",
-        };
-
-        jest.spyOn(adminsRepository, "findByID").mockResolvedValueOnce(admin);
-
-        const repositorySpy = jest.spyOn(adminsRepository, "delete");
-
-        await sut.execute({ id: "valid_id" });
-
-        expect(repositorySpy).toHaveBeenCalledWith("valid_id");
-    });
-
-    it("Should throws if throw", async () => {
-        const { sut } = makeSut();
-
-        jest.spyOn(sut, "execute").mockImplementationOnce(() => {
-            throw new Error();
-        });
-
-        await expect(async () => {
-            await sut.execute({ id: "valid_id" });
-        }).rejects.toThrow();
+            await deleteAdminUseCase.execute({ id: admin.id });
+        }).rejects.toBeInstanceOf(AppError);
     });
 });
