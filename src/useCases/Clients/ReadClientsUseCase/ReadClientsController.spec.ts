66f311f3a7ba9de9ad8d5cf80f2e1d1d3e820c0d
Herik Santos <heriksantos04@gmail.com>
2024-07-20 12:19:42 -0300

    test: creation test

--- src/useCases/Clients/ReadClientsUseCase/ReadClientsController.spec.ts
+++ src/useCases/Clients/ReadClientsUseCase/ReadClientsController.spec.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/consistent-type-assertions */
 import { type Response, type Request } from "express";
 
+import { AppError } from "../../../errors/AppErros";
 import { ClientsRepositoryMemory } from "../../../repositories/ClientsRepositoryMemory";
 import { ReadClientsController } from "./ReadClientsController";
 import { ReadClientsUseCase } from "./ReadClientsUseCase";
@@ -26,5 +27,20 @@ describe("List all clients", () => {
 
         // eslint-disable-next-line @typescript-eslint/unbound-method
         expect(mockResponse.status).toHaveBeenCalledWith(200);
+    });
+
+    it("Should throw if name is not string or is undefined", async () => {
+        const mockRequest = {} as Request;
+        mockRequest.query = { name: ["123", "abc"] };
+
+        const mockResponse = {} as Response;
+        mockResponse.status = jest.fn().mockReturnThis();
+        mockResponse.json = jest.fn();
+
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        await expect(
+            async () =>
+                await readClientsController.handle(mockRequest, mockResponse),
+        ).rejects.toEqual(new AppError("Invalid type", 400));
     });
 });
--- src/useCases/Clients/ReadClientsUseCase/ReadClientsUseCase.spec.ts
+++ src/useCases/Clients/ReadClientsUseCase/ReadClientsUseCase.spec.ts
@@ -1,15 +1,26 @@
 import { ClientsRepositoryMemory } from "../../../repositories/ClientsRepositoryMemory";
+import { type IClientsRepository } from "../../../repositories/IClientsRepository";
 import { ReadClientsUseCase } from "./ReadClientsUseCase";
 
-let clientsRepositoryMemory: ClientsRepositoryMemory;
-let readClientsUseCase: ReadClientsUseCase;
+interface ITypeSut {
+    sut: ReadClientsUseCase;
+    clientsRepository: IClientsRepository;
+}
+
+function makeSut(): ITypeSut {
+    const clientsRepository = new ClientsRepositoryMemory();
+    const sut = new ReadClientsUseCase(clientsRepository);
+
+    return {
+        sut,
+        clientsRepository,
+    };
+}
 
 describe("Should be list all clients", () => {
-    beforeEach(() => {
-        clientsRepositoryMemory = new ClientsRepositoryMemory();
-        readClientsUseCase = new ReadClientsUseCase(clientsRepositoryMemory);
-    });
     it("List all client", async () => {
+        const { sut, clientsRepository } = makeSut();
+
         const client = {
             name: "herik",
             qtd_cortes: 2,
@@ -21,11 +32,11 @@ describe("Should be list all clients", () => {
             data_nasc: new Date("2001-04-21"),
         };
 
-        await clientsRepositoryMemory.create(client);
+        await clientsRepository.create(client);
 
-        await clientsRepositoryMemory.create(client2);
+        await clientsRepository.create(client2);
 
-        const findedClient = await readClientsUseCase.execute({
+        const findedClient = await sut.execute({
             name: client.name,
         });
 
