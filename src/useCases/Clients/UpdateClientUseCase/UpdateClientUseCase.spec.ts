2f2c6a3f82748a003c029416b684a786a39bf3c4
Herik Santos <heriksantos04@gmail.com>
2024-07-22 11:03:30 -0300

    test: should throws if id is undefined

--- src/useCases/Clients/UpdateClientUseCase/UpdateClientUseCase.spec.ts
+++ src/useCases/Clients/UpdateClientUseCase/UpdateClientUseCase.spec.ts
@@ -2,47 +2,74 @@ import { v4 as uuidv4 } from "uuid";
 
 import { AppError } from "../../../errors/AppErros";
 import { ClientsRepositoryMemory } from "../../../repositories/ClientsRepositoryMemory";
+import { type IClientsRepository } from "../../../repositories/IClientsRepository";
 import { UpdateClientUseCase } from "./UpdateClientUseCase";
 
-let clientsRepositoryMemory: ClientsRepositoryMemory;
-let updateClientUseCase: UpdateClientUseCase;
+interface ITypeSut {
+    sut: UpdateClientUseCase;
+    clientsRepository: IClientsRepository;
+}
+
+function makeSut(): ITypeSut {
+    const clientsRepository = new ClientsRepositoryMemory();
+    const sut = new UpdateClientUseCase(clientsRepository);
+
+    return {
+        sut,
+        clientsRepository,
+    };
+}
 
 describe("Should be edit client", () => {
-    beforeEach(() => {
-        clientsRepositoryMemory = new ClientsRepositoryMemory();
-        updateClientUseCase = new UpdateClientUseCase(clientsRepositoryMemory);
-    });
-
     it("Edit client", async () => {
+        const { sut, clientsRepository } = makeSut();
+
         const client = {
             name: "cobaia",
             data_nasc: new Date("2001-04-02"),
             qtd_cortes: 5,
         };
 
-        await clientsRepositoryMemory.create(client);
+        await clientsRepository.create(client);
 
-        const findedClient = await clientsRepositoryMemory.findOne(client);
+        const findedClient = await clientsRepository.findOne(client);
 
         if (!findedClient) throw new AppError("Client not found");
 
         findedClient.name = "Teste123";
 
-        const editedClient = await updateClientUseCase.execute(findedClient);
+        const editedClient = await sut.execute(findedClient);
 
         expect(editedClient.name).toBe("Teste123");
     });
 
     it("Should be impossible edit a client nonexisting", async () => {
-        await expect(async () => {
-            const client = {
-                id: uuidv4(),
-                name: "cobaia",
-                data_nasc: new Date("2001-04-02"),
-                qtd_cortes: 5,
-            };
+        const { sut } = makeSut();
 
-            await updateClientUseCase.execute(client);
-        }).rejects.toBeInstanceOf(AppError);
+        const client = {
+            id: uuidv4(),
+            name: "cobaia",
+            data_nasc: new Date("2001-04-02"),
+            qtd_cortes: 5,
+        };
+
+        await expect(async () => {
+            await sut.execute(client);
+        }).rejects.toEqual(new AppError("Client not found", 400));
+    });
+
+    it("Should throws if id is undefined", async () => {
+        const { sut } = makeSut();
+
+        const client = {
+            id: "",
+            name: "cobaia",
+            data_nasc: new Date("2001-04-02"),
+            qtd_cortes: 5,
+        };
+
+        await expect(async () => {
+            await sut.execute(client);
+        }).rejects.toEqual(new AppError("Id is required", 400));
     });
 });
